56f9be3e7bdabb86b3f40ac5141763a1d5e60f64
Hadrien HABBAK <hhabbak@student.42.fr>
2019-03-30 18:49:11 +0100

    add door

---
+++ rush/Assets/Scripte/Door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+        rb.centerOfMass = Vector3.zero;
+	}
+	
+	void Update () {
+		
+	}
+}
