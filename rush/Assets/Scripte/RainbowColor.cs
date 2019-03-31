3edf47213ac4fe32d1c8ee8509e3ab2421c12c3a
Hadrien HABBAK <hhabbak@student.42.fr>
2019-03-31 19:54:55 +0200

    change rainbow colors

--- rush/Assets/Scripte/RainbowColor.cs
+++ rush/Assets/Scripte/RainbowColor.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class RainbowColor : MonoBehaviour
 {
+    public List<Color> listColors;
+    int i;
+    float t;
+
+    void Start()
+    {
+        // listColors = new List<Color>();
+        // listColors.Add(Color.red);
+        // listColors.Add(Color.blue);
+        // listColors.Add(Color.green);
+        i = 0;
+    }
+
     void Update()
     {
-        Camera.main.backgroundColor = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1.0f));
+        Color nexColor = listColors[(i + 1) % listColors.Count];
+        t += Time.deltaTime;
+        Camera.main.backgroundColor = Color.Lerp(listColors[i], nexColor, t);
+        if (t >= 1.0f)
+        {
+            i++;
+            if (i == listColors.Count)
+                i = 0;
+            t = 0.0f;
+        }
     }
 }
