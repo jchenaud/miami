6cb162668863ff985ad4306fea4108d30d2e5ece
Hadrien HABBAK <hhabbak@student.42.fr>
2019-03-31 19:20:59 +0200

    update titlescreen

--- rush/Assets/Scripte/TitleScreen.cs
+++ rush/Assets/Scripte/TitleScreen.cs
@@ -6,6 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
+    public Texture2D cursor;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+    }
+
     public void PlayButton()
     {
         SceneManager.LoadScene(1);
