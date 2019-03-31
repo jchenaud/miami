dd197085c23c9074960dc7467814ede3f4e17c74
Hadrien HABBAK <hhabbak@student.42.fr>
2019-03-31 17:00:09 +0200

    add endCar script

---
+++ rush/Assets/Scripte/EndCar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCar : MonoBehaviour {
+	bool moveCar;
+
+	void Start ()
+	{
+		Player.onWinGameEvent += OnWin;
+	}
+	
+	void Update ()
+	{
+		if (moveCar)
+			transform.position += -transform.up * Time.deltaTime * 4.25f;
+	}
+
+	void OnWin()
+	{
+		Debug.Log("test");
+		moveCar = true;
+	}
+}
